88e9de0813bb61c3cdab641d9a76fc58629ae80a
meetpatel2902 <meetpatel292003@gmail.com>
2025-05-30 13:06:56 +0530

    all changes done!

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/api/auth', authRoutes);
 app.use('/api/notes', noteRoutes);
-app.use('/api/users', userRoutes);
 app.use('/api/admin', adminRoutes);
 
 const io = new Server(server, {
