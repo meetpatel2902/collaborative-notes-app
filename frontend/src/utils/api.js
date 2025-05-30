6eca7a2842364ca373ad6a676844a292a9e6b2ed
meetpatel2902 <meetpatel292003@gmail.com>
2025-05-30 12:47:37 +0530

    Triggering new deployment after vercel.json setup

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env._APP_BACKEND_URL;
+const API_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
     baseURL: API_URL,
