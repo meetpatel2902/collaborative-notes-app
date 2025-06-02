e904832b2d3c4c8e1730748dc18b9493e9c42239
meetpatel2902 <meetpatel292003@gmail.com>
2025-06-02 12:01:55 +0530

    all changes done!

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,23 +1,46 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_BACKEND_URL;
 
 const api = axios.create({
-   baseURL: `${API_URL}/api`, 
+    baseURL: `${API_URL}/api`,
     headers: {
         'Content-Type': 'application/json',
     },
+    withCredentials: true,
 });
 
-
-api.interceptors.request.use((config) => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.token) {
-        config.headers.Authorization = `Bearer ${user.token}`;
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
-});
+);
+
+
+api.interceptors.response.use(
+    (response) => {
+        
+        if (response.data.token) {
+            localStorage.setItem('token', response.data.token);
+        }
+        return response;
+    },
+    (error) => {
+        
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem('token'); 
+            
+            window.location.href = '/login'; 
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default api;
