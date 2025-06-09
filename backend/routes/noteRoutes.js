624cafa4a0bdf96b925dd0ac660a964170373e6f
meetpatel2902 <meetpatel292003@gmail.com>
2025-06-09 11:23:46 +0530

    all changes done!

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -5,31 +5,17 @@ const {
     createNote,
     updateNote,
     deleteNote,
-<<<<<<< HEAD
 } = require('../controllers/noteController');
-const { protect, admin } = require('../middlewares/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.route('/').get(protect, getNotes).post(protect, createNote);
+router.route('/')
+    .get(protect, getNotes)
+    .post(protect, createNote);
 router.route('/:id')
     .get(protect, getNoteById)
     .put(protect, updateNote)
     .delete(protect, deleteNote);
 
-=======
-} = require('../controllers/noteController'); 
-console.log('getNotes:', getNotes);
-const { protect, checkAdmin } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.route('/')
-    .get(protect, getNotes) 
-    .post(protect, createNote);
-router.route('/:id')
-    .get(protect, getNoteById) 
-    .put(protect, updateNote) 
-    .delete(protect, deleteNote); 
->>>>>>> 26c5641088948f08b786bad3489b6a599a7c3caf
 module.exports = router;
